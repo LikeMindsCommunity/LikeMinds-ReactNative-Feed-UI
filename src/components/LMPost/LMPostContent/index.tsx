974448f9de072d30765eeba20e0b8f8a69867c2a
Prabha <prabha.yadav@likeminds.community>
2023-09-30 22:50:33 +0530

    fixed show more functionality in ios

--- src/components/LMPost/LMPostContent/index.tsx
+++ src/components/LMPost/LMPostContent/index.tsx
@@ -1,57 +1,81 @@
-import {
-  Dimensions,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View
-} from 'react-native';
-import React, {useEffect, useState} from 'react';
-import { LMPostContentProps } from './types';
-import STYLES from '../../../constants/constants'
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {LMPostContentProps} from './types';
+import STYLES from '../../../constants/constants';
+import {MAX_DEFAULT_POST_CONTENT_LINES} from '../../../constants/strings';
 
-const LMPostContent = ({text, linkData, textStyle, linkStyle, visibleLines, showMoreTextStyle, postContentViewStyle}: LMPostContentProps) => {
-  const [showMoreVisible, setShowMoreVisible] = useState(false);
-  const [numberOfLine, setNumOfLine] = useState(0);
-  // this handles the visibility of show more button
+const LMPostContent = ({
+  text,
+  linkData,
+  textStyle,
+  linkStyle,
+  visibleLines,
+  showMoreTextStyle,
+  postContentViewStyle,
+}: LMPostContentProps) => {
+  const MAX_LINES = visibleLines
+    ? visibleLines
+    : MAX_DEFAULT_POST_CONTENT_LINES;
+  const [showText, setShowText] = useState(false);
+  const [numberOfLines, setNumberOfLines] = useState<number>();
+  const [showMoreButton, setShowMoreButton] = useState(false);
+
+  // this handles the show more functionality
+  const onTextLayout = useCallback(
+    (e: any) => {
+      if (e.nativeEvent.lines.length > MAX_LINES && !showText) {
+        setShowMoreButton(true);
+        setNumberOfLines(MAX_LINES);
+      }
+    },
+    [showText],
+  );
+
+  // this handles the visiblity of whole post content and trimmed text upto maximum line
   useEffect(() => {
-    setShowMoreVisible(false);
-    // this checks the condition if the text takes more than given visible lines or have more than 500 characters
-    if (
-      (Dimensions.get('screen').width * (visibleLines ? (visibleLines-1) : 2)) / 6.71 < text?.length ||
-      text?.length > 500
-    ) {
-      setNumOfLine(visibleLines? visibleLines : 3);
-      setShowMoreVisible(true);
-    } else {
-      setShowMoreVisible(false);
+    if (showMoreButton) {
+      setNumberOfLines(showText ? undefined : MAX_LINES);
     }
-  }, [text]);
+  }, [showText, showMoreButton]);
+
+  // this renders the attached link's urls
+  const renderLinks = () => {
+    return linkData?.map(item => {
+      return (
+        <Text
+          style={StyleSheet.flatten([styles.linkText, linkStyle])}
+          key={item.attachmentMeta.ogTags.url}>
+          {item.attachmentMeta.ogTags.url}
+        </Text>
+      );
+    });
+  };
 
   return (
     <View style={StyleSheet.flatten([postContentViewStyle])}>
+      {/* post content text */}
       <Text
-        numberOfLines={numberOfLine}
-        style={StyleSheet.flatten([styles.contentText, textStyle])}>
-        {showMoreVisible ? text?.substring(0, 500) : text}
+        style={StyleSheet.flatten([styles.contentText, textStyle])}
+        onTextLayout={onTextLayout}
+        numberOfLines={numberOfLines}>
+        {text}
       </Text>
       {/* link urls section */}
-      {
-        showMoreVisible ? null : 
-        linkData?.map((item) =>{
-          return(
-            <Text style={StyleSheet.flatten([styles.linkText,linkStyle])} key={item.attachmentMeta.ogTags.url}>{item.attachmentMeta.ogTags.url}</Text>
-          )
-        })
-      }
+      {showMoreButton ? (
+        showText ? (
+          <>{renderLinks()}</>
+        ) : null
+      ) : (
+        <>{renderLinks()}</>
+      )}
       {/* show more button section */}
-      {showMoreVisible && (
+      {showMoreButton && (
         <TouchableOpacity
-          onPress={() => {
-            setShowMoreVisible(false);
-            setNumOfLine(0);
-          }}>
+          disabled={showText ? true : false}
+          onPress={() => setShowText(showText => !showText)}
+          accessibilityRole="button">
           <Text style={StyleSheet.flatten([showMoreTextStyle])}>
-            Show more
+            {showText ? '' : 'Show More'}
           </Text>
         </TouchableOpacity>
       )}
@@ -59,15 +83,14 @@ const LMPostContent = ({text, linkData, textStyle, linkStyle, visibleLines, show
   );
 };
 
-
 const styles = StyleSheet.create({
-  contentText:{
-      fontSize: 16,
-      color: STYLES.$COLORS.TEXT_COLOR,
+  contentText: {
+    fontSize: 15,
+    color: STYLES.$COLORS.TEXT_COLOR,
   },
-  linkText:{
-      color:'#007AFF',
-  }
+  linkText: {
+    color: '#007AFF',
+  },
 });
 
 export default LMPostContent;
