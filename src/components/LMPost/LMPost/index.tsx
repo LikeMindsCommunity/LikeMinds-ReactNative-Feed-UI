93b4827487b73eea67b8eff1eab1124bbf80bf20
Prabha <prabha.yadav@likeminds.community>
2023-10-03 17:39:36 +0530

    resolved comments

--- src/components/LMPost/LMPost/index.tsx
+++ src/components/LMPost/LMPost/index.tsx
@@ -4,37 +4,28 @@ import LMPostHeader from '../LMPostHeader';
 import LMPostContent from '../LMPostContent';
 import LMPostMedia from '../LMPostMedia';
 import LMPostFooter from '../LMPostFooter';
+import {LMPostProps} from './types';
 
-const LMPost = ({
-  id,
-  user,
-  menuItems,
-  text,
-  attachments,
-  isLiked,
-  isSaved,
-  likesCount,
-  commentsCount,
-}: LMPostUI) => {
+const LMPost = ({post}: LMPostProps) => {
   return (
     <View>
       {/* post header */}
       <LMPostHeader
-        user={user}
-        menuItems={menuItems}
+        user={post.user}
+        menuItems={post.menuItems}
         // todo: make a callback function to return the id of menu item clicked and execute further functionality according to it
         onMenuItemTap={() => {}}
       />
       {/* post content */}
-      <LMPostContent text={text} />
+      <LMPostContent text={post.text} />
       {/* post media */}
-      <LMPostMedia attachments={attachments} />
+      <LMPostMedia attachments={post.attachments} />
       {/* post footer */}
       <LMPostFooter
-        isLiked={isLiked}
-        isSaved={isSaved}
-        likesCount={likesCount}
-        commentsCount={commentsCount}
+        isLiked={post.isLiked}
+        isSaved={post.isSaved}
+        likesCount={post.likesCount}
+        commentsCount={post.commentsCount}
       />
     </View>
   );
