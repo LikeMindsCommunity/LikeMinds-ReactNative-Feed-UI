2dfcdea0f2e922207cc496d71d54ed685310f4c0
Prabha <prabha.yadav@likeminds.community>
2023-10-06 11:51:31 +0530

    changed index file

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import LMText from './base/LMText';
 import LMProfilePicture from './base/LMProfilePicture';
 import LMInputText from './base/LMInputText';
 import { LMImage, LMVideo, LMCarousel, LMDocument, LMLinkPreview } from './components/LMMedia';
-import { LMPostHeader, LMPostFooter , LMPostMedia, LMPostContent} from './components/LMPost';
+import { LMPostHeader, LMPostFooter , LMPostMedia, LMPostContent, LMPost} from './components/LMPost';
 import LMMemberListItem from './components/LMMemberListItem'
 import LMNotificationFeedItem from './components/LMNotificationFeedItem';
 import LMCommentItem from './components/LMCommentItem';
 
-export {LMText, LMIcon, LMButton, LMProfilePicture, LMInputText,LMImage, LMVideo, LMCarousel, LMDocument, LMLinkPreview, LMPostHeader, LMPostFooter, LMPostMedia, LMPostContent, LMMemberListItem, LMNotificationFeedItem, LMCommentItem};
+export {LMText, LMIcon, LMButton, LMProfilePicture, LMInputText,LMImage, LMVideo, LMCarousel, LMDocument, LMLinkPreview, LMPostHeader, LMPostFooter, LMPostMedia, LMPostContent, LMMemberListItem, LMNotificationFeedItem, LMCommentItem, LMPost};
